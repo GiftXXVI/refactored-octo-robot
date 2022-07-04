4360ba0a30c096f01f6c97ad6bb41477d5e9c501
GiftXXVI <gift.chimphonda@outlook.com>
2022-07-04 16:05:22 +0200

    Added GitIgnore

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 from flask import Flask
-
+from model import setup_db
 app = Flask(__name__)
+db, app = setup_db(app)
--- model.py
+++ model.py
@@ -1,16 +1,15 @@
-from enum import unique
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import null
 
 db = SQLAlchemy()
 
 
 def setup_db(app):
     global db
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://test.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    return db
+    return db, app
 
 
 class User(db.Model):
